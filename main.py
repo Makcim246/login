da0fe76c07df0580a2b8b5e335c4c76e6e30fb9d
T1 <kovalevichm246@gmail.com>
2023-01-22 16:24:23 +0300

    New commit server

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import socket
+import http.server
+import socketserver
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 8000))
-server.listen(1)
+port = 8080
+Handler = http.server.SimpleHTTPRequestHandler
 
-while True:
-    conn, addr = server.accept()
-    print(f'Connection server: {addr}')
-    request = conn.recv(1024).decode('utf-8')
-    print(request)
-    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charseet=utf-8\r\n\r\n'
-    response = 'test'.encode('utf-8')
-    conn.send(HDRS.encode('utf-8') + response)
-    conn.close()
-    print('Connection closed\n')
+with socketserver.TCPServer(("", port), Handler) as httpd:
+    print("Сервер запущен!\nПорт: ", port)
+    print("http://localhost:8080/")
+    httpd.serve_forever()
